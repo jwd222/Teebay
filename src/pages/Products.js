00e39d612ee507c09d2488fc31cd236a96c19059
jwd222 <sji.2.sami@gmail.com>
2023-08-19 14:52:23 +0600

    added buttons on my products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,19 +3,27 @@ import { Product } from '../components'
 
 const Products = () => {
   return (
-    <div className="d-flex flex-column align-items-center h-100 justify-content-center">
-      <h3>All products</h3>
-      {productList.map((product) => {
-        return (
-          <Product
-            title={product.title}
-            price={product.price}
-            description={product.description}
-            date={product.date}
-          />
-        )
-      })}
-    </div>
+    <>
+      <div className="d-flex flex-row-reverse m-4">
+        <button className="btn btn-danger">Logout</button>
+      </div>
+      <div className="d-flex flex-column align-items-center h-100 justify-content-center">
+        <h3>All products</h3>
+        {productList.map((product) => {
+          return (
+            <Product
+              title={product.title}
+              price={product.price}
+              description={product.description}
+              date={product.date}
+            />
+          )
+        })}
+        <div className="d-flex m-4 flex-row-reverse">
+          <button className="btn btn-primary">Add Product</button>
+        </div>
+      </div>
+    </>
   )
 }
 export default Products
