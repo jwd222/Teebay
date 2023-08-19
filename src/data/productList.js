2c1a721caaf33964bcd07e61bb8740b6f258faf6
jwd222 <sji.2.sami@gmail.com>
2023-08-19 14:13:16 +0600

    added dummy data for frontend "My Products" page

---
+++ src/data/productList.js
@@ -0,0 +1,18 @@
+const productList = [
+  {
+    title: 'Football boot',
+    // categories: [],
+    price: '$500',
+    description: 'Football boot for sale',
+    date: '11.11.11',
+  },
+  {
+    title: 'Football',
+    // categories: [],
+    price: '$400',
+    description: 'Football for sale',
+    date: '13.12.11',
+  },
+]
+
+export default productList
