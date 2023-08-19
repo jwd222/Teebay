ea3cc9b5b630646687d03eb1bd61c8b068a8ec87
jwd222 <sji.2.sami@gmail.com>
2023-08-19 19:47:03 +0600

    implemented and added modal(bugged)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,20 @@
-const Product = ({ title, price, description, date }) => {
+import Modal from './Modal'
+
+const Product = ({ id, title, price, description, date }) => {
   return (
-    <div className="product-card card d-flex justify-content-center align-content-center my-2">
-      <div class="card-body">
-        <div className="d-flex justify-content-between">
-          <h5 class="card-title">{title}</h5>
-          <button className="btn btn-default">Click me</button>
+    <>
+      <div className="product-card card d-flex justify-content-center align-content-center my-2">
+        <div className="card-body">
+          <div className="d-flex justify-content-between">
+            <h5 className="card-title">{title}</h5>
+            <Modal />
+          </div>
+          <p className="card-text">{description}</p>
+          <p className="card-text">{price}</p>
+          <p className="card-text">{date}</p>
         </div>
-        <p class="card-text">{description}</p>
-        <p class="card-text">{price}</p>
-        <p class="card-text">{date}</p>
       </div>
-    </div>
+    </>
   )
 }
 export default Product
