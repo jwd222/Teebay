b303a39da4a9663bd20bc89b2667f694b752f1b3
jwd222 <sji.2.sami@gmail.com>
2023-08-22 04:41:00 +0600

    used card component for product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,6 @@
-import Modal from './Modal'
+import Card from './Card'
 
-const Product = ({ id, title, price, description, date }) => {
-  return (
-    <>
-      <div className="product-card card d-flex justify-content-center align-content-center my-2">
-        <div className="card-body">
-          <div className="d-flex justify-content-between">
-            <h5 className="card-title">{title}</h5>
-            <Modal />
-          </div>
-          <p className="card-text">{description}</p>
-          <p className="card-text">{price}</p>
-          <p className="card-text">{date}</p>
-        </div>
-      </div>
-    </>
-  )
+const Product = ({ ...data }) => {
+  return <Card {...data} />
 }
 export default Product
