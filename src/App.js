c0a26d946595790a4cd0224852343cc2e4f8aeed
jwd222 <sji.2.sami@gmail.com>
2023-08-22 04:37:24 +0600

    added edit product route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Register, Products, AddProduct } from './pages'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Register, Products, AddProduct, EditProduct } from './pages'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/dashboard" element={<Products />} />
         <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/edit-product" element={<EditProduct />} />
       </Routes>
     </BrowserRouter>
   )
