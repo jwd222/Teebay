12fc4e20c2d0809f46445e06fb38e5b0c33d299c
jwd222 <sji.2.sami@gmail.com>
2023-08-23 05:58:25 +0600

    added date in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,39 @@
 import { Link } from 'react-router-dom'
-// import Modal from './Modal'
 
-const Card = ({ title, category, description, buyPrice, rentPrice }) => {
+const Card = ({
+  key,
+  title,
+  category,
+  description,
+  buyPrice,
+  rentPrice,
+  createdAt,
+}) => {
+  // console.log(typeof Number(createdAt))
+  // let date = new Date(Number(createdAt)).toLocaleString()
+  // console.log(date.toLocaleDateString('en-US'))
   return (
     <div className="product-card card d-flex justify-content-center align-content-center my-2">
       <div className="card-body">
         <div className="d-flex justify-content-between">
-          <h5 className="card-text">Title: {title}</h5>
-          {/* <Modal /> */}
+          {/* <h5 className="card-text">Title: {title}</h5> */}
+          <Link
+            to="/edit-product"
+            className="card-title"
+            state={{ key, title, category, description, buyPrice, rentPrice }}
+          >
+            Title: {title}
+          </Link>
         </div>
         <div className="mt-2" style={{ transform: 'rotate(0)' }}>
           <p className="card-text">Category: {category}</p>
           <p className="card-text">Description: {description}</p>
           <p className="card-text">Price: ${buyPrice}</p>
           <p className="card-text">Rent: ${rentPrice}</p>
-          {/* <p className="card-text">Date: {date}</p> */}
-          <Link to="/edit-product" className="stretched-link" />
+          <p className="card-text">
+            Created at: {new Date(Number(createdAt)).toLocaleString()}
+          </p>
+          {/* <Link to="/edit-product" /> */}
         </div>
       </div>
     </div>
