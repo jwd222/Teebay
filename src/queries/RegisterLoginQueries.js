70acefda6a14d94184860d7db52beb26a6c09792
jwd222 <sji.2.sami@gmail.com>
2023-08-22 20:31:02 +0600

    added query to get products from userId

--- src/queries/RegisterLoginQueries.js
+++ src/queries/RegisterLoginQueries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 // queries
-// export const GetUserQuery = gql`
+// export const GET_USER = gql`
 //   query GetUser($getUserId: ID!) {
 //     getUser(id: $getUserId) {
 //       firstName
@@ -11,14 +11,26 @@ import { gql } from '@apollo/client'
 //   }
 // `
 
-export const GetUserIdQuery = gql`
+export const GET_USERID = gql`
   query ExampleQuery($email: String!, $password: String!) {
     getUserId(email: $email, password: $password)
   }
 `
 
+export const GET_PRODUCT_FROM_USERID = gql`
+  query ExampleQuery($ownerId: ID) {
+    getProductsFromId(ownerId: $ownerId) {
+      title
+      category
+      description
+      buyPrice
+      rentPrice
+    }
+  }
+`
+
 // mutations
-export const CreateUserQuery = gql`
+export const CREATE_USER = gql`
   mutation CreateUser(
     $firstName: String!
     $email: String!
