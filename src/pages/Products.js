2a073bf06879d76a8ab440bc789dfb3a58c30a26
jwd222 <sji.2.sami@gmail.com>
2023-08-22 22:27:40 +0600

    fetched products with query and displayed in Products component, also implemented logout

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,30 +1,65 @@
-import productList from '../data/productList'
-import { Modal, Product } from '../components'
+// import productList from '../data/productList'
+import { Product } from '../components'
 import { Link } from 'react-router-dom'
 
+import { GET_PRODUCT_FROM_USERID } from '../queries/RegisterLoginQueries'
+import { useLazyQuery, useQuery } from '@apollo/client'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Products = () => {
+  const navigate = useNavigate()
+  const userId = localStorage.getItem('userId')
+  const getProductFromUserIdData = useQuery(GET_PRODUCT_FROM_USERID, {
+    variables: {
+      ownerId: userId,
+    },
+  })
+  let [products, setProducts] = useState([])
 
+  useEffect(() => {
+    if (getProductFromUserIdData.data) {
+      // console.log(getProductFromUserIdData.data)
+      const { getProductsFromId } = getProductFromUserIdData?.data
+      setProducts([...getProductsFromId])
+      console.log(getProductsFromId)
+      // localStorage.setItem('products', JSON.stringify(newProductList))
+    }
+  }, [getProductFromUserIdData.data])
+  // const newProductList = JSON.parse(localStorage.getItem('products'))
+  // console.log(newProductList)
 
-  
+  const logOut = () => {
+    localStorage.removeItem('userId')
+    navigate('/')
+  }
+
   return (
     <>
       <div className="d-flex flex-row-reverse m-4">
-        <button className="btn btn-danger">Logout</button>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            logOut()
+          }}
+        >
+          Logout
+        </button>
       </div>
       <div className="d-flex flex-column align-items-center h-100 justify-content-center">
         <h3>All products</h3>
-        {productList.map((product, index) => {
+        {products.map((product, index) => {
+          // console.log(product)
           return (
-            <>
-              <Modal />
-              <Product
-                key={index}
-                title={product.title}
-                price={product.buyPrice}
-                description={product.description}
-                date={product.date}
-              />
-            </>
+            <Product
+              key={index}
+              title={product.title}
+              buyPrice={product.buyPrice}
+              rentPrice={product.rentPrice}
+              description={product.description}
+              category={product.category}
+              // date={product.date}
+            />
           )
         })}
         <div className="d-flex m-4 flex-row-reverse">
